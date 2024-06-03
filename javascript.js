c46f060f2aa47c0052ecbaf940008fcecba887c5
HP <rawan.manswor@gmail.com>
2024-06-04 00:38:14 +0300

    glass website

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let x = " rawan ";
+let y = 27;
+console.log("my name" + x +"age is " + y);
+console.log(`my name ${x} and my age is ${y}`)
+console.log(typeof(x + 5))
+console.log(typeof(1+1))
+console.log(typeof('1'-'1'))
+console.log('5' ** '5')
+console.log('u' - 4)
